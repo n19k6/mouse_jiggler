5dde7bc99e691c6f458e9aaf2232cb4723bdfe4d
n19k6 <46622208+n19k6@users.noreply.github.com>
2021-09-28 09:07:43 +0200

    Update boot.py

--- pico_port/boot.py
+++ pico_port/boot.py
@@ -15,7 +15,7 @@ button.pull = digitalio.Pull.DOWN
 # Disable devices only if button is not pressed.
 if not button.value:
     print("test")
-    #storage.disable_usb_drive()
-    #usb_cdc.disable()
+    storage.disable_usb_drive()
+    usb_cdc.disable()
 
 usb_hid.enable((usb_hid.Device.MOUSE,))
